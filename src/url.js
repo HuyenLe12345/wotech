a217d79aa94baa04de87b3e51342c28f45b44012
HuyenLe12345 <168576422+HuyenLe12345@users.noreply.github.com>
2025-01-08 17:28:31 +0700

    Update url.js

--- src/url.js
+++ src/url.js
@@ -1,1 +1,9 @@
 export const base_url = "https://wotech-server-2.onrender.com";
+export const isValidURL = (string) => {
+  try {
+    new URL(string);
+    return true;
+  } catch (_) {
+    return false;
+  }
+};

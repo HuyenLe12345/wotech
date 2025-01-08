b7c9b4849aacf15a0e3b1cb4f3d7ee576690172e
HuyenLe12345 <168576422+HuyenLe12345@users.noreply.github.com>
2025-01-08 17:53:47 +0700

    Delete src directory

--- src/ErrorPage.js
+++
@@ -1,8 +0,0 @@
-const ErrorPage = (props) => {
-  return (
-    <p style={{ textAlign: "center", fontSize: "20px", marginTop: "30px" }}>
-      {props.error}
-    </p>
-  );
-};
-export default ErrorPage;
--- src/url.js
+++
@@ -1,9 +0,0 @@
-export const base_url = "https://wotech-server-2.onrender.com";
-export const isValidURL = (string) => {
-  try {
-    new URL(string);
-    return true;
-  } catch (_) {
-    return false;
-  }
-};
